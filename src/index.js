624927a0e549fe78d583937776607eefc291e038
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2020-10-02 22:16:42 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n.toString().split('').splice(1).join('');
+    } else {
+        n = n.toString()
+    }
+
+    return n.split('').reverse().join('');
 }
